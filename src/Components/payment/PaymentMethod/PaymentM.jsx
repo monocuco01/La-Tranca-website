11a0f07e1cbef8c4a8cc33f49e6fefc40ac3c1ba
monocuco01 <119557633+monocuco01@users.noreply.github.com>
2024-04-01 15:57:49 -0500

    finish

--- src/Components/payment/PaymentMethod/PaymentM.jsx
+++ src/Components/payment/PaymentMethod/PaymentM.jsx
@@ -6,11 +6,11 @@ import Other from "./payment-methods-svgrepo-com.svg";
 
 const PaymentM = () => {
   const nequiLink =
-    "https://wa.me/3244707930?text=¡Hola!%20Bienvenido/a%20a%20Tranca%20Café.%20Para%20poder%20procesar%20tu%20pedido,%20¿podrías%20proporcionarme%20la%20siguiente%20información%3F%0A%0ADirección%20de%20entrega%3A%0ANombre%20completo%3A%0AProductos%20que%20deseas%20ordenar%3A%0ANúmero%20de%20teléfono%20de%20contacto%3A%0A%0A¡Gracias%20por%20tu%20ayuda!";
+    "https://wa.me/3244707930?text=¡Hola!%20Quisiera%20realizar%20un%20pedido.";
   const cashLink =
-    "https://wa.me/3244707930?text=¡Hola!%20Bienvenido/a%20a%20Tranca%20Café.%20Para%20poder%20procesar%20tu%20pedido,%20¿podrías%20proporcionarme%20la%20siguiente%20información%3F%0A%0ADirección%20de%20entrega%3A%0ANombre%20completo%3A%0AProductos%20que%20deseas%20ordenar%3A%0ANúmero%20de%20teléfono%20de%20contacto%3A%0A%0A¡Gracias%20por%20tu%20ayuda!";
+    "https://wa.me/3244707930?text=¡Hola!%20Quisiera%20realizar%20un%20pedido.";
   const otherLink =
-    "https://wa.me/3244707930?text=¡Hola!%20Bienvenido/a%20a%20Tranca%20Café.%20Para%20poder%20procesar%20tu%20pedido,%20¿podrías%20proporcionarme%20la%20siguiente%20información%3F%0A%0ADirección%20de%20entrega%3A%0ANombre%20completo%3A%0AProductos%20que%20deseas%20ordenar%3A%0ANúmero%20de%20teléfono%20de%20contacto%3A%0A%0A¡Gracias%20por%20tu%20ayuda!";
+    "https://wa.me/3244707930?text=¡Hola!%20Quisiera%20realizar%20un%20pedido.";
 
   return (
     <div className="containerPaymentM">
